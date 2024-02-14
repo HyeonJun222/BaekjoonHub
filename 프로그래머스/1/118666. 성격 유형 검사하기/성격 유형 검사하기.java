d97a2ca0dbf88eea27f75d8ead380a33f69497a4
HyeonJun222 <151013766+HyeonJun222@users.noreply.github.com>
2024-02-14 17:31:35 +0900

    [level 1] Title: 성격 유형 검사하기, Time: 0.31 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/118666. 성격 유형 검사하기/성격 유형 검사하기.java
@@ -0,0 +1,43 @@
+class Solution {
+    public String solution(String[] survey, int[] choices) {
+        StringBuilder answer = new StringBuilder();
+        char[] alp = new char[]{'R', 'T', 'C', 'F', 'J', 'M', 'A', 'N'};
+        int[] mbti = new int[4];    //RT CF JM AN
+        for (int i = 0; i < choices.length; i++) {
+            if (survey[i].equals("NA")) {
+                mbti[3] -= (choices[i] - 4);
+            }
+            if (survey[i].equals("AN")) {
+                mbti[3] += (choices[i] - 4);
+            }
+            if (survey[i].equals("MJ")) {
+                mbti[2] -= (choices[i] - 4);
+            }
+            if (survey[i].equals("JM")) {
+                mbti[2] += (choices[i] - 4);
+            }
+            if (survey[i].equals("FC")) {
+                mbti[1] -= (choices[i] - 4);
+            }
+            if (survey[i].equals("CF")) {
+                mbti[1] += (choices[i] - 4);
+            }
+            if (survey[i].equals("TR")) {
+                mbti[0] -= (choices[i] - 4);
+            }
+            if (survey[i].equals("RT")) {
+                mbti[0] += (choices[i] - 4);
+            }
+        }
+        for (int j = 0; j < 4; j++) {
+            if (mbti[j] < 0) {
+                answer.append(alp[j * 2]);
+            } else if (mbti[j] > 0) {
+                answer.append(alp[j * 2 + 1]);
+            } else {
+                answer.append(alp[j * 2]);
+            }
+        }
+        return answer.toString();
+    }
+}
